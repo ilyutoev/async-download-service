100512414bee88ee1ab537cf106896845c334497
ilyutoev <ilyutoev@gmail.com>
2020-08-26 23:56:51 +0300

    Базовое вью, которое отдает заархивированный файл частями

--- server.py
+++ server.py
@@ -1,12 +1,37 @@
+import asyncio
+
 from aiohttp import web
 import aiofiles
 
+BASE_PHOTOS_FOLDER = 'test_photos'
+
 
 async def archivate(request):
-    raise NotImplementedError
+    """Функция, которая архивирует переданую в запросе папку и возвращает ответ клиенту по частям."""
+    response = web.StreamResponse()
+
+    archive_hash = request.match_info.get('archive_hash')
+    response.headers['Content-Disposition'] = f'attachment; filename="{archive_hash}.zip"'
+
+    await response.prepare(request)
+
+    proc = await asyncio.create_subprocess_shell(f'zip -rj - {BASE_PHOTOS_FOLDER}/{archive_hash}',
+                                                 stdout=asyncio.subprocess.PIPE)
+
+    while True:
+        archive_part = await proc.stdout.read(500 * 1024)
+        if archive_part:
+            await response.write(archive_part)
+        else:
+            break
+
+    await response.write_eof()
+
+    return response
 
 
 async def handle_index_page(request):
+    """Статическая главная страница."""
     async with aiofiles.open('index.html', mode='r') as index_file:
         index_contents = await index_file.read()
     return web.Response(text=index_contents, content_type='text/html')
